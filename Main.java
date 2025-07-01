7fe0d41fd4ba903da20af5dc9f4f9ed336ea0526
Aromalpriyan <aromalpriyan213@gmail.com>
2025-07-01 14:46:05 +0530

    Completed

--- Main.java
+++ Main.java
@@ -1,8 +1,3 @@
-# Assignement Java - 13
-## Question
-### <p>1. Given an array of n integers count the number of elements that have at least one elements greater than itself?<p>
-## Output
-```
 public class Main {
     public static void main(String[] args) {
         int[] arr = {3,1,4,5,4,2};
@@ -25,5 +20,3 @@ public class Main {
             System.out.println("Number of elements with at least one element greater " + result);
     }
 }
-```
-[githublink]()
